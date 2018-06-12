ce3d2eb9b5832d8b41219d99b7220a7f678b2090
dpfrakes <dpfrakes@gmail.com>
2018-06-12 10:52:04 -0400

    Idempotent summary csv writer, refactor

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const pa11y = require('pa11y');
 const fs = require('fs');
 const csvWriter = require('csv-write-stream');
+const csvParser = require('csv-parser');
 const slugify = require('slugify');
 
 runAccessibilityTest();
 
+function writeSummary(content) {
+    content.sort((a, b) => a.URL > b.URL)
+    let writer = csvWriter({ headers: ['URL', 'Errors', 'Warnings', 'Notices', 'Last Update']});
+    writer.pipe(fs.createWriteStream('summary.csv'));
+    content.forEach((row) => writer.write(row));
+    writer.end();
+}
+
 async function runAccessibilityTest() {
+
     try {
+
+        // Runtime
+        let datetime = new Date();
 
         // Read URLs from [Jenkins] env variable
         let urls = process.env.A11Y_URLS.split('\n');
@@ -18,6 +31,7 @@ async function runAccessibilityTest() {
 
         // Global options
         let options = {
+            'timeout': 60000,
             'includeWarnings': true,
             'includeNotices': true
         };
@@ -25,38 +39,43 @@ async function runAccessibilityTest() {
         // Run tests against multiple URLs
         let results = await Promise.all(urls.map((url) => pa11y(url, options)));
 
-        // Write summary results to CSV
-        let summaryWriter = csvWriter({ headers: ['URL', 'Errors', 'Warnings', 'Notices']})
-        summaryWriter.pipe(fs.createWriteStream('summary.csv'))
-
         // Save summary to return to Jenkins job for email notification
         let summary = [];
 
         results.map((r) => {
 
             // Write single row summary for URL
-            let summaryRow = [
-                r.pageUrl,
-                r.issues.filter((i) => i.typeCode === 1).length,
-                r.issues.filter((i) => i.typeCode === 2).length,
-                r.issues.filter((i) => i.typeCode === 3).length
-            ]
-
-            summaryWriter.write(summaryRow);
-            summary.push(summaryRow)
+            summary.push({
+                'URL': r.pageUrl,
+                'Errors': r.issues.filter((i) => i.typeCode === 1).length,
+                'Warnings': r.issues.filter((i) => i.typeCode === 2).length,
+                'Notices': r.issues.filter((i) => i.typeCode === 3).length,
+                'Last Update': datetime.toLocaleString()
+            });
 
             // Write complete report for URL
             let reportWriter = csvWriter()
-            reportWriter.pipe(fs.createWriteStream(`reports/${slugify(r.documentTitle)}.csv`))
+            reportWriter.pipe(fs.createWriteStream(`reports/${slugify(r.documentTitle)}.csv`));
             for(i = 0; i < r.issues.length; i++) {
-                reportWriter.write(r.issues[i])
+                reportWriter.write(r.issues[i]);
             }
-            reportWriter.end()
+            reportWriter.end();
+        });
 
-        })
-
-        summaryWriter.end()
-        return summary;
+        // If summary.csv already exists, read it and update only the necessary lines
+        if (fs.existsSync('./summary.csv')) {
+            fs.createReadStream('summary.csv')
+                .pipe(csvParser())
+                .on('data', function (data) {
+                    // For each line, if new audit doesn't include previous URL, use previous URL
+                    if (!summary.filter((row) => row.URL == data.URL)[0]) {
+                        summary.push(data);
+                    }
+                })
+                .on('end', () => writeSummary(summary));
+        } else {
+            writeSummary(summary);
+        }
 
     } catch (error) {
         console.error(error.message);
